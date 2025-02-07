49ed9f3afbd18e4447bc2e5ab4b40f733302bb65
armandrausell <armandrausell@gmail.com>
2025-02-07 15:58:35 +0100

    Visualize species overlaps

---
+++ VisSpeciesOverlap.R
@@ -0,0 +1,13 @@
+
+VisOverlap <- function(species_A, species_B) {
+  # Get rasters for both species
+  raster_A <- plot_species(species_A) #plot_species is created in AtlasData.R
+  raster_B <- plot_species(species_B)
+  
+  # Sum both rasters (cells with 2 indicate overlap)
+  combined_raster <- raster_A + raster_B
+  plot(combined_raster)
+}
+
+VisOverlap("Ursus arctos", "Pyrrhocorax graculus")
+
