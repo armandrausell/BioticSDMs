c0434bdb687897b604bf4d378869a2c4127d23fe
armandrausell <armandrausell@gmail.com>
2025-04-10 12:49:36 +0200

    Update

--- RelativePredatorPressure.R
+++ RelativePredatorPressure.R
@@ -1,11 +1,79 @@
+setwd("~/Desktop/Github/BioticSDMs")
+
+df<-read.csv2("metaweb/TetraEU_pairwise_interactions.csv", )
+
+#Load the names created in GetCompleteSpeciesList.RData
+load(file = "Atlas_data/Complete_sp_list_iberia.RData") #all_sp object
+
+
+# Load required library
+library(dplyr)
+
+# Get unique species names
+species <- unique(c(df$sourceTaxonName, df$targetTaxonName))
+species<-sort(species)
+
+# Create an empty binary matrix
+binary_matrix <- matrix(0, nrow = length(species), ncol = length(species), 
+                        dimnames = list(species, species))
+
+# Fill the matrix based on interactions
+for (i in 1:nrow(df)) {
+  predator <- df$sourceTaxonName[i]
+  prey <- df$targetTaxonName[i]
+  binary_matrix[predator, prey] <- 1
+}
+
+# Convert to data frame for better readability
+binary_matrix_df <- as.data.frame(binary_matrix)
+
+# Compute the shared predation matrix
+shared_predation_matrix <- binary_matrix %*% t(binary_matrix)
+
+# Convert to dataframe for better readability
+shared_predation_df <- as.data.frame(shared_predation_matrix)
+
+# Get predator species names
+predators <- rownames(shared_predation_df)
+
+# Get the total number of prey each predator eats (diagonal values)
+total_prey <- diag(as.matrix(shared_predation_df))
+total_prey_df <- data.frame(Predator = predators, TotalPrey = total_prey)
+
+# Count how many times each species is a prey
+prey_counts <- colSums(binary_matrix_df)
+
+# Convert to dataframe
+prey_df <- data.frame(
+  Species = names(prey_counts),
+  Times_Preyed_On = as.integer(prey_counts)
+)
+
+# Filter from the total list the species that are in Spain
+prey_count_spain<-prey_df %>% filter(Species %in% all_sp)
+predator_count_spain<- total_prey_df %>% filter(Predator %in% all_sp)
+
+proportion_prey_predator<-cbind(prey_count_spain,predator_count_spain)
+proportion_prey_predator<-proportion_prey_predator[,-3]
+
+#This creates a column with predatory ranking
+proportion_prey_predator$TrophicPosition<-
+  (proportion_prey_predator$Times_Preyed_On+1)/(proportion_prey_predator$TotalPrey+1)*-1
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))} # Range standardization 0-1
+
+proportion_prey_predator$TrophicPosition<- range01(proportion_prey_predator$TrophicPosition)
+
+#This will give us an order of species according the trophic position they occupy
+proportion_prey_predator$TrophicPosition<-rank(proportion_prey_predator$TrophicPosition)
 
 
 # Calculation of interaction strenghts prey-predator from binary matrix
 
 # Filter the matrix to only include species present in names_sps (both rows and columns)
 binary_matrix_filtered <- binary_matrix_df[
-  rownames(binary_matrix_df) %in% names_sps,
-  colnames(binary_matrix_df) %in% names_sps
+  rownames(binary_matrix_df) %in% all_sp,
+  colnames(binary_matrix_df) %in% all_sp
 ]
 
 # Trophic level with NetIndices
@@ -49,6 +117,7 @@ trophic_info_tl <- tl %>%
 trophic_info_prop <- proportion_prey_predator %>%
   dplyr::select(Species, TrophicPosition)
 
+trophic_info<-trophic_info_prop
 trophic_info$TrophicPosition<-trophic_info_prop$TrophicPosition*trophic_info_tl$TL
 # Join TrophicPosition for predators
 predation_pairs_tp <- predation_pairs %>%
@@ -119,13 +188,6 @@ predation_pairs_tp1 <- predation_pairs_tp1 %>%
 #######################################################
 # Calculate Relative Predator Pressure (rPP)
 Relative_pred_pressure <- predation_pairs_tp1 %>%
-  mutate(
-    rPP = (abs(TrophicPositionPred - TrophicPositionPrey) * (1 / TotalPrey_Pred)
-           * TrophicCoreProximity) / 
-      (Times_Prey_Preyed + 1)
-  )
-
-Relative_test <- predation_pairs_tp1 %>%
   mutate(
     rPP = (log2(abs(TrophicPositionPred - TrophicPositionPrey)) * 
              (1 / (1+TotalPrey_Pred*(1-TrophicCoreProximity)))) 
